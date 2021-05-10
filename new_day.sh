c602df5aabb72384913c46ea7e652af9c90446d8
Grant Horner <h.grant.horner@gmail.com>
2021-05-09 21:42:41 -0400

    fix script

--- new_day.sh
+++ new_day.sh
@@ -8,11 +8,11 @@ touch "$folder_path/input.txt"
 touch "$folder_path/sample.txt"
 
 echo "pub fn part_one() -> std::io::Result<i32> {
-    let file = std::fs::File::open("/Users/grant/Dev/rust-advent/src/day_$1/sample.txt")?;
+    let file = std::fs::File::open(\"/Users/grant/Dev/rust-advent/src/day_$1/sample.txt\")?;
     Ok(-1)
 }
 
 pub fn part_two() -> std::io::Result<i32> {
-    let file = std::fs::File::open("/Users/grant/Dev/rust-advent/src/day_$1/sample.txt")?;
+    let file = std::fs::File::open(\"/Users/grant/Dev/rust-advent/src/day_$1/sample.txt\")?;
     Ok(-1)
 }" > "$folder_path/mod.rs"
--- src/day_three/mod.rs
+++ src/day_three/mod.rs
@@ -1,9 +1,9 @@
 pub fn part_one() -> std::io::Result<i32> {
-    let file = std::fs::File::open(/Users/grant/Dev/rust-advent/src/day_three/sample.txt)?;
+    let input = std::fs::read_to_string("/Users/grant/Dev/rust-advent/src/day_three/sample.txt")?;
     Ok(-1)
 }
 
 pub fn part_two() -> std::io::Result<i32> {
-    let file = std::fs::File::open(/Users/grant/Dev/rust-advent/src/day_three/sample.txt)?;
+    let file = std::fs::File::open("/Users/grant/Dev/rust-advent/src/day_three/sample.txt")?;
     Ok(-1)
 }
