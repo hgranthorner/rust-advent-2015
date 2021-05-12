90357f9db2bb47be1ed74c6a0671c454f7533746
Grant Horner <h.grant.horner@gmail.com>
2021-05-12 19:27:24 -0400

    Completed part one

--- src/day_three/mod.rs
+++ src/day_three/mod.rs
@@ -1,6 +1,43 @@
+use std::convert::TryInto;
+
+enum Direction {
+    Up   ,
+    Left ,
+    Down ,
+    Right
+}
+
+impl Direction {
+    pub fn from_char(c: char) -> Option<Direction> {
+        match c {
+            '^' => Some(Direction::Up),
+            '<' => Some(Direction::Left),
+            'v' => Some(Direction::Down),
+            '>' => Some(Direction::Right),
+            _ => None
+        }
+    }
+}
+
 pub fn part_one() -> std::io::Result<i32> {
-    let input = std::fs::read_to_string("/Users/grant/Dev/rust-advent/src/day_three/sample.txt")?;
-    Ok(-1)
+    let input = std::fs::read_to_string("/Users/grant/Dev/rust-advent/src/day_three/input.txt")?;
+    let mut positions = std::collections::HashSet::new();
+    let mut current_pos = (0, 0);
+    positions.insert(current_pos);
+    for c in input.chars() {
+        if let Some(dir) = Direction::from_char(c) {
+            let (x, y) = current_pos;
+            current_pos = match dir {
+                Direction::Up => (x, y + 1),
+                Direction::Left => (x - 1, y),
+                Direction::Down => (x, y - 1),
+                Direction::Right => (x + 1, y)
+            };
+            positions.insert(current_pos);
+        }
+    }
+
+    Ok(positions.len().try_into().unwrap())
 }
 
 pub fn part_two() -> std::io::Result<i32> {
