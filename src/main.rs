31571fea9c90abfe873c49c5ff279cd018bc540b
Grant Horner <h.grant.horner@gmail.com>
2021-05-13 21:35:27 -0400

    Set up for day 4

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod day_one;
 mod day_two;
 mod day_three;
+mod day_four;
 
 fn main() {
-    match day_three::part_two() {
+    match day_four::part_one() {
         Ok(v) => println!("{}", v),
         Err(e) => println!("{:?}", e)
     }
