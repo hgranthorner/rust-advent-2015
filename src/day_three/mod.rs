2881a2f0a2d847acb2416af57bbba5d6966aa322
Grant Horner <h.grant.horner@gmail.com>
2021-05-13 21:32:01 -0400

    Finished day 3

--- src/day_three/mod.rs
+++ src/day_three/mod.rs
@@ -41,6 +41,35 @@ pub fn part_one() -> std::io::Result<i32> {
 }
 
 pub fn part_two() -> std::io::Result<i32> {
-    let file = std::fs::File::open("/Users/grant/Dev/rust-advent/src/day_three/sample.txt")?;
-    Ok(-1)
+    let input = std::fs::read_to_string("/Users/grant/Dev/rust-advent/src/day_three/input.txt")?;
+    let mut santa = std::collections::HashSet::new();
+    let mut robot = std::collections::HashSet::new();
+    let mut santa_position = (0, 0);
+    let mut robot_position = (0, 0);
+    santa.insert(santa_position);
+    for ( i, c ) in input.chars().enumerate() {
+        if i % 2 == 0 {
+            santa_position = move_and_record(&mut santa, santa_position, c);
+        } else {
+            robot_position = move_and_record(&mut robot, robot_position, c);
+        }
+    }
+
+    let total = santa.union(&robot).collect::<Vec<_>>().len();
+    Ok(total.try_into().unwrap())
+}
+
+fn move_and_record(sleigh: &mut std::collections::HashSet<(i32, i32)>, current_pos: (i32, i32), c: char) -> (i32, i32) {
+    let (x, y) = current_pos;
+    if let Some(dir) = Direction::from_char(c) {
+        let pos = match dir {
+            Direction::Up => (x, y + 1),
+            Direction::Left => (x - 1, y),
+            Direction::Down => (x, y - 1),
+            Direction::Right => (x + 1, y)
+        };
+        sleigh.insert(pos);
+        return pos;
+    }
+    current_pos
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod day_two;
 mod day_three;
 
 fn main() {
-    match day_three::part_one() {
+    match day_three::part_two() {
         Ok(v) => println!("{}", v),
         Err(e) => println!("{:?}", e)
     }
